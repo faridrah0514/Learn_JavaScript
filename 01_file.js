63bf138da0ddf9a8d111622a1eda7da43bd5b3cd
faridrah0514 <fr.farid.rahman@gmail.com>
2019-11-27 19:50:23 +0700

    first commit to learn javascript

---
+++ 01_file.js
@@ -0,0 +1,20 @@
+console.log("Halo")
+// document.write('Hallo World');
+// console.log(__dirname);
+// console.log(__filename)
+
+var nama = 'Andi'
+console.log(nama)
+
+var usia
+usia = 22
+usia = 23
+console.log(usia)
+
+let jomblo = true
+// let jomblo = "false dong";
+console.log(jomblo)
+
+var kabar = prompt("Apa Kabar? ")
+console.log(kabar)
+alert(kabar)
---
+++ 03_String_and_number.js
@@ -0,0 +1,34 @@
+var nama = 'Andi'
+var usia = 22
+let jomblo = true
+let kerja
+
+console.log(typeof(nama))
+console.log(typeof(usia))
+console.log(typeof(jomblo))
+console.log(typeof(kerja))
+console.log(kerja)
+
+var x = "Hallo Dunia bahasa indonesia"
+console.log(x.length)
+console.log(x.indexOf('Dunia'))
+console.log(x.substr(5, 3))
+console.log(x.slice(5,10))
+console.log(x.split(' '))
+
+var z = 12345
+console.log(z.toString())
+
+let a = new Date()
+console.log(typeof(a))
+
+var x = 4
+var y = 3
+var z = 2
+var w
+w=((x+(y*z))/(x*y))
+console.log(Math.pow(w,z))
+
+console.log(Math.pow(5,2))
+console.log(4**2)
+console.log(5%3)
